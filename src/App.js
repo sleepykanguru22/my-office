e2ed02b2572e38f87bdb3bbe235ff4b78b22a68a
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-02-16 15:31:58 -0800

    testing spline react options

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from "@splinetool/react-spline";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Spline scene="https://prod.spline.design/2s7hXEhwU3FR8k6N/scene.splinecode" />
+      <h1>Testing testing testing testing testing</h1>
     </div>
   );
 }
